466c83598f97676dd88cf69844d8586f005f3259
fukbarsik <ksamorodov@yandex.ru>
2020-09-19 17:39:19 +0300

    add Collider and GameObject classes

---
+++ Platformerengine/res/code/logic/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformerengine.res.code.logic {
+    class Point {
+        private int X { get; }
+        private int Y { get; }
+        public Point(int _x, int _y) {
+            X = _x;
+            Y = _y; 
+        }
+        public Point(Point _p) {
+            X = _p.X;
+            Y = _p.Y;
+        }
+    }
+}
