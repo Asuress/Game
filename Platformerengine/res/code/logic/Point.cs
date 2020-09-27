5c7f3aea569b3dca45b37336822e32092b2d1aba
Vet Kos <gvcfhnkoo@gmail.com>
2020-09-27 17:37:01 +0300

    namespace

--- Platformerengine/res/code/logic/Point.cs
+++ Platformerengine/res/code/logic/Point.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Platformerengine.res.code {
+namespace Platformerengine.res.code.logic {
  class Point
     {
         private double X { get; set; }
