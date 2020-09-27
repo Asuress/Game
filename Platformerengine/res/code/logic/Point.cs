4cff6129df7db71f7b5e7f36d5ed7f9a88e506e4
Vet Kos <gvcfhnkoo@gmail.com>
2020-09-27 17:01:16 +0300

    Point

--- Platformerengine/res/code/logic/Point.cs
+++ Platformerengine/res/code/logic/Point.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Platformerengine.res.code.logic {
     class Point {
-        private int X { get; }
-        private int Y { get; }
-        public Point(int _x, int _y) {
+        private double X { get;private set; }
+        private double Y { get; private set; }
+        public Point(double _x = 0, double _y = 0) {
             X = _x;
             Y = _y; 
         }
@@ -16,5 +16,35 @@ namespace Platformerengine.res.code.logic {
             X = _p.X;
             Y = _p.Y;
         }
+        public override bool Equals(object any){
+            if(any is Point){
+                return Equals(this, any)
+            }
+            return false;
+        }
+        public static bool Equals(Point p1, Point p2){
+            if(p1.X == p2.X && p1.Y == p2.Y){
+                return true;
+            }
+            return false;
+        }
+        public Subtract(Point end){
+            return new IVector2d(end.X - this.X,end.Y - this.Y)
+        }
+        public Move(double _x,double _y){
+            X+= _x;
+            Y+= _y;
+
+        }
+        public static Point operator + (Point p1,Point p2){
+            return new Point(p1.X + p2.X, p1.Y + p2.Y)
+        }
+        public static Point operator - (Point p1, Point p2){
+            return new Point(p1.X - p2.X, p1.Y - p2.Y) 
+        }
+        public static bool operator !=(Point p1, Vector2 p2) => !(p1.X == p1.X && p1.Y == p2.Y);
+        public static bool operator ==(Point p1, Point p2) => (p1.X == p2.X && p1.Y == p2.Y);
     }
 }
+
+
