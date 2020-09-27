713059602efd475734472af2351a6c18ab86a7f7
Vet Kos <gvcfhnkoo@gmail.com>
2020-09-27 17:28:02 +0300

    Point update

--- Platformerengine/res/code/logic/Point.cs
+++ Platformerengine/res/code/logic/Point.cs
@@ -5,46 +5,57 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Platformerengine.res.code.logic {
-    class Point {
-        private double X { get;private set; }
-        private double Y { get; private set; }
-        public Point(double _x = 0, double _y = 0) {
-            X = _x;
-            Y = _y; 
-        }
-        public Point(Point _p) {
-            X = _p.X;
-            Y = _p.Y;
-        }
-        public override bool Equals(object any){
-            if(any is Point){
-                return Equals(this, any)
+ class Point
+        {
+            private double X { get; private set; }
+            private double Y { get; private set; }
+            public Point(double _x = 0, double _y = 0)
+            {
+                X = _x;
+                Y = _y;
             }
-            return false;
-        }
-        public static bool Equals(Point p1, Point p2){
-            if(p1.X == p2.X && p1.Y == p2.Y){
-                return true;
+            public Point(Point _p)
+            {
+                X = _p.X;
+                Y = _p.Y;
             }
-            return false;
-        }
-        public Subtract(Point end){
-            return new IVector2d(end.X - this.X,end.Y - this.Y)
-        }
-        public Move(double _x,double _y){
-            X+= _x;
-            Y+= _y;
+            public override bool Equals(object any)
+            {
+                if (any is Point)
+                {
+                    return Equals(this, any);
+                }
+                return false;
+            }
+            public static bool Equals(Point p1, Point p2)
+            {
+                if (p1.X == p2.X && p1.Y == p2.Y)
+                {
+                    return true;
+                }
+                return false;
+            }
+            public Vector2 Subtract(Point end)
+            {
+                return new Vector2(end.X - this.X, end.Y - this.Y);
+            }
+            public void Move(double _x, double _y)
+            {
+                X += _x;
+                Y += _y;
 
+            }
+            public static Point operator +(Point p1, Point p2)
+            {
+                return new Point(p1.X + p2.X, p1.Y + p2.Y);
+            }
+            public static Point operator -(Point p1, Point p2)
+            {
+                return new Point(p1.X - p2.X, p1.Y - p2.Y);
+            }
+            public static bool operator !=(Point p1, Vector2 p2) => !(p1.X == p1.X && p1.Y == p2.Y);
+            public static bool operator ==(Point p1, Point p2) => (p1.X == p2.X && p1.Y == p2.Y);
         }
-        public static Point operator + (Point p1,Point p2){
-            return new Point(p1.X + p2.X, p1.Y + p2.Y)
-        }
-        public static Point operator - (Point p1, Point p2){
-            return new Point(p1.X - p2.X, p1.Y - p2.Y) 
-        }
-        public static bool operator !=(Point p1, Vector2 p2) => !(p1.X == p1.X && p1.Y == p2.Y);
-        public static bool operator ==(Point p1, Point p2) => (p1.X == p2.X && p1.Y == p2.Y);
-    }
 }
 
 
