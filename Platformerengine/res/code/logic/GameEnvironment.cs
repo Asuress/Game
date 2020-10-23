40ab32eeeee37d58f548d3d68e457d6e4b6ea113
Maxim <maxim.kryuchkov1@gmail.ru>
2020-10-23 16:40:38 +0300

    Added GameEnvironment class

---
+++ Platformerengine/res/code/logic/GameEnvironment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Platformerengine.res.code.logic
+{
+    public abstract class GameEnvironment
+    {
+        public GameEnvironment()
+        {
+            CompositionTarget.Rendering += GameLoop;
+        }
+
+        private void GameLoop(object sender, EventArgs e)
+        {
+            EarlyUpdate();
+            Update();
+            LateUpdate();
+        }
+
+        protected virtual void EarlyUpdate() { }
+        protected virtual void Update() { }
+        protected virtual void LateUpdate() { }
+    }
+}
