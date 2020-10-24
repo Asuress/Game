8bad1d3e7d0e906fe98aeee560981e6f0c786a23
Maxim <maxim.kryuchkov1@gmail.ru>
2020-10-24 10:39:34 +0300

    Scene

---
+++ Platformerengine/res/code/Graphics/Scene.cs
@@ -0,0 +1,24 @@
+﻿using Platformerengine.res.code.logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformerengine.res.code.Graphics
+{
+    public class Scene
+    {
+        Scene()
+        {
+
+        }
+
+        public LinkedList<GameObject> Objects { get; }
+
+        public void AddGameObject(GameObject go)
+        {
+
+        }
+    }
+}
