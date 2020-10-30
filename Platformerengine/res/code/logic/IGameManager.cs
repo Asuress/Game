db574b304233991b1fd6616345736c8aa6b4e5c8
Maxim <maxim.kryuchkov1@gmail.ru>
2020-10-30 15:56:43 +0300

    Fixed Scene and IManagers, Added collider & boxcollider classes

--- Platformerengine/res/code/logic/IGameManager.cs
+++ Platformerengine/res/code/logic/IGameManager.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Windows;
 
 namespace Platformerengine.res.code.logic {
-    interface IGameManager {
+    public interface IGameManager {
         void StartPosition(Point start);
         void Update();
         void End();
