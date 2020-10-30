0b0f122bba7f12ce35d08fa5b941174db58dda82
Maxim <maxim.kryuchkov1@gmail.ru>
2020-10-30 15:50:51 +0300

    Fixed scene class & managers interface, added Collider & BoxCollider classes

---
+++ Platformerengine/res/code/physics/BoxCollider.cs
@@ -0,0 +1,86 @@
+﻿using Platformerengine.res.code.graphic;
+using Platformerengine.res.code.logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformerengine.res.code.physics
+{
+    public class BoxCollider : Collider
+    {
+
+        public BoxCollider(GameObject parent, Scene scene) : base(parent)
+        {
+            Size = parent.Transform.Size;
+            _Scene = scene;
+        }
+        public BoxCollider(GameObject parent, Scene scene, Size size) : base(parent)
+        {
+            Size = size;
+            _Scene = scene;
+        }
+
+        public Size Size { get; set; }
+
+        bool IsStay = false;
+        Scene _Scene;
+
+        protected override void EarlyUpdate()
+        {
+            LinkedList<GameObject> intersectsWith = new LinkedList<GameObject>();
+            foreach (var go in _Scene.ListOfObj)
+            {
+                Sides side;
+                if (IntersectWith(go, out side) && go != parent)
+                {
+                    intersectsWith.AddLast(go);
+                    foreach (var intersect in intersectsWith)
+                    {
+                        if (IsStay && intersect == go)
+                        {
+                            InvokeOnCollisionStay(go.Collider, side);
+                        }
+                        else
+                        {
+                            IsStay = true;
+                            InvokeOnCollisionEnter(go.Collider, side);
+                        }
+                    }
+                }
+                else if (!IntersectWith(go, out side) && go != parent)
+                {
+                    foreach (var intersect in intersectsWith)
+                    {
+                        if (IsStay && intersect == go)
+                        {
+                            IsStay = false;
+                            InvokeOnCollisionExit(go.Collider, side);
+                        }
+                    }
+                }
+            }
+        }
+
+        private bool IntersectWith(GameObject gameObject, out Sides side)
+        {
+            if (Math.Abs(parent.Transform.Position.X - gameObject.Transform.Position.X) <= (parent.Transform.Size.Width + gameObject.Transform.Size.Width) / 2 &&
+               Math.Abs(parent.Transform.Position.Y - gameObject.Transform.Position.Y) <= (parent.Transform.Size.Height + gameObject.Transform.Size.Height) / 2)
+            {
+                side = default;
+                if (parent.Transform.Position.X - gameObject.Transform.Position.X >= 0)
+                    side = Sides.West;
+                else if (parent.Transform.Position.X - gameObject.Transform.Position.X <= 0)
+                    side = Sides.East;
+                else if (parent.Transform.Position.Y - gameObject.Transform.Position.Y > 0)
+                    side = Sides.North;
+                else if (parent.Transform.Position.Y - gameObject.Transform.Position.Y > 0)
+                    side = Sides.South;
+                return true;
+            }
+            side = default;
+            return false;
+        }
+    }
+}
--- Platformerengine/res/code/physics/Collider.cs
+++ Platformerengine/res/code/physics/Collider.cs
@@ -6,15 +6,44 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Platformerengine.res.code.physics {
-    class Collider : Physics {
-        private int Size { get; set; }
-        private Point Coord { get; set; }
-        public Collider(int _size, Point _p) {
-            Size = _size;
-            Coord = new Point(_p);
+    public abstract class Collider : GameEnvironment
+    {
+        public enum Sides { Default = 0, North, West, South, East }
+        public class ColliderEventArgs
+        {
+            public ColliderEventArgs(Collider collider, Sides side)
+            {
+                Side = side;
+                Collider = collider;
+            }
+            public Collider Collider { get; }
+            public Sides Side { get; }
         }
-        public void UpdateCoord(Point _coord) {
-            Coord = _coord;
+
+        public delegate void ColliderEventHandler(ColliderEventArgs colliderArgs);
+
+        public virtual event ColliderEventHandler OnCollisionEnter = delegate { };
+        public virtual event ColliderEventHandler OnCollisionExit = delegate { };
+        public virtual event ColliderEventHandler OnCollisionStay = delegate { };
+
+        protected Collider(GameObject parent)
+        {
+            this.parent = parent;
+        }
+
+        protected readonly GameObject parent;
+
+        protected void InvokeOnCollisionEnter(Collider collider, Sides side)
+        {
+            OnCollisionEnter(new ColliderEventArgs(collider, side));
+        }
+        protected void InvokeOnCollisionStay(Collider collider, Sides side)
+        {
+            OnCollisionStay(new ColliderEventArgs(collider, side));
+        }
+        protected void InvokeOnCollisionExit(Collider collider, Sides side)
+        {
+            OnCollisionExit(new ColliderEventArgs(collider, side));
         }
     }
 }
