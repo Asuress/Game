7ef38835edb46e1eafb8524ef570e94bec83c41f
Maxim <maxim.kryuchkov1@gmail.ru>
2020-12-21 07:04:41 +0300

    animator

---
+++ Platformerengine/res/code/graphic/Animator.cs
@@ -0,0 +1,40 @@
+﻿using Platformerengine.res.code.logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Platformerengine.res.code.graphic
+{
+    public class Animator : GameEnvironment
+    {
+        public Animator(GameObject parent, int duration, List<Brush> images)
+        {
+            Images = images;
+            Parent = parent;
+            Duration = duration;
+            Frame = 0;
+            CurrentImageIndex = 0;
+        }
+        List<Brush> Images;
+        GameObject Parent;
+        int Duration;
+        int CurrentImageIndex;
+        int Frame;
+        protected override void LateUpdate()
+        {
+            Frame++;
+            if(Frame == Duration/Images.Count)
+            {
+                Frame = 0;
+                Parent.Shape.Fill = Images[CurrentImageIndex];
+                CurrentImageIndex++;
+                if (CurrentImageIndex == Images.Count)
+                    CurrentImageIndex = 0;
+            }
+        }
+    }
+}
--- Platformerengine/res/game_res/game_code/Coin.cs
+++ Platformerengine/res/game_res/game_code/Coin.cs
@@ -1,13 +1,17 @@
-﻿using Platformerengine.res.code.logic;
+﻿using Platformerengine.res.code.graphic;
+using Platformerengine.res.code.logic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 
 namespace Platformerengine.res.game_res.game_code {
     class Coin : GameObject {
+        
         public CoinScript Script { get; set; }
         public Coin(string id, Point point) : base(id, "Coin", new Ellipse(), point, new Size(50, 50)) {
             Script = new CoinScript();
@@ -20,7 +24,14 @@ namespace Platformerengine.res.game_res.game_code {
 
             shape.Fill = resourses.Sprites["bronze_2.jpg"];
             Shape = shape;
+            List<Brush> images = new List<Brush>();
+            images.Add(resourses.Sprites["bronze_1.jpg"]);
+            images.Add(resourses.Sprites["bronze_2.jpg"]);
+            images.Add(resourses.Sprites["bronze_3.jpg"]);
+            anim = new Animator(this, 50, images);
+            anim.Start();
+            
         }
-
+        Animator anim;
     }
 }
